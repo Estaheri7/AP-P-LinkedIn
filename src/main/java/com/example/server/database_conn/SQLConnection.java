44e94b06a8e8132a845c4daf24c357a71b7b9052
erfansalman <estaheri101@gmail.com>
2024-05-10 23:33:22 +0330

    add: added Connection to database using singleton design pattern

--- src/main/java/com/example/server/database_conn/SQLConnection.java
+++ src/main/java/com/example/server/database_conn/SQLConnection.java
@@ -1,4 +1,25 @@
 package com.example.server.database_conn;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/linkedin";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    private static Connection conn = null;
+
+    private SQLConnection() {
+        // Singleton design pattern
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (conn == null || conn.isClosed()) {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+
+        return conn;
+    }
 }
