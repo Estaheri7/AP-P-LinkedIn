d460821d0fada159616e2869bc258bce4c2d5d09
erfansalman <estaheri101@gmail.com>
2024-05-11 22:13:57 +0330

    bugfix: fixed MySQL syntax in table query

--- src/main/java/com/example/server/database_conn/CommentDB.java
+++ src/main/java/com/example/server/database_conn/CommentDB.java
@@ -11,14 +11,14 @@ public class CommentDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
-        String query = "CREATE DATABASE IF NOT EXISTS comments ("
+        String query = "CREATE TABLE IF NOT EXISTS comments ("
                 + "id INT AUTO_INCREMENT PRIMARY KEY,"
                 + "post_id INT NOT NULL,"
                 + "email VARCHAR(255) NOT NULL,"
                 + "comment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
                 + "message VARCHAR(1250) NOT NULL,"
                 + "FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE,"
-                + "FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE,"
+                + "FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE"
                 + ");";
 
         Statement statement = conn.createStatement();
