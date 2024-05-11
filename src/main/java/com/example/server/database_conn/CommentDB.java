4d3c9390adbd1a96638b24e831af79aac96a18ac
erfansalman <estaheri101@gmail.com>
2024-05-11 21:46:19 +0330

    add: added createTable method in CommentDB

--- src/main/java/com/example/server/database_conn/CommentDB.java
+++ src/main/java/com/example/server/database_conn/CommentDB.java
@@ -11,6 +11,16 @@ public class CommentDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE DATABASE IF NOT EXISTS comments ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "post_id INT NOT NULL,"
+                + "email VARCHAR(255) NOT NULL,"
+                + "comment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+                + "message VARCHAR(1250) NOT NULL,"
+                + "FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
