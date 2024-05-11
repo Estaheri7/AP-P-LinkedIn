6b0cdf0911170d69e91bdcc1c371af01b11d8b19
erfansalman <estaheri101@gmail.com>
2024-05-11 21:54:38 +0330

    add: added LikeDB class

---
+++ src/main/java/com/example/server/database_conn/LikeDB.java
@@ -0,0 +1,15 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class LikeDB extends BaseDB {
+
+    public LikeDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
