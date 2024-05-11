87781fcbf0155e42fc7865efdee11494331c8a00
erfansalman <estaheri101@gmail.com>
2024-05-11 21:45:22 +0330

    add: added CommentDB class

---
+++ src/main/java/com/example/server/database_conn/CommentDB.java
@@ -0,0 +1,16 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CommentDB extends BaseDB {
+
+    public CommentDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
