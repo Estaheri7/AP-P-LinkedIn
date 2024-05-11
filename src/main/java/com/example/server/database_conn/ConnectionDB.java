0be07b76ae51a66383359b2c113e16a487c6d9d9
erfansalman <estaheri101@gmail.com>
2024-05-11 18:06:30 +0330

    add: added ConnectionDB class

---
+++ src/main/java/com/example/server/database_conn/ConnectionDB.java
@@ -0,0 +1,15 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class ConnectionDB extends BaseDB {
+
+    public ConnectionDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
