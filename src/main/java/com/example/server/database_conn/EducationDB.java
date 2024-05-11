42a1a729f32fba258f592697a99a725b7391d038
erfansalman <estaheri101@gmail.com>
2024-05-11 16:01:55 +0330

    add: created EducationDB class

---
+++ src/main/java/com/example/server/database_conn/EducationDB.java
@@ -0,0 +1,16 @@
+package com.example.server.database_conn;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class EducationDB extends BaseDB {
+
+    public EducationDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
