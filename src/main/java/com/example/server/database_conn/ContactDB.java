d420e1df964582fd1d30d620381c551fad584ada
erfansalman <estaheri101@gmail.com>
2024-05-11 16:32:55 +0330

    refactor: removed NOT NULL statement for some columns

--- src/main/java/com/example/server/database_conn/ContactDB.java
+++ src/main/java/com/example/server/database_conn/ContactDB.java
@@ -13,12 +13,12 @@ public class ContactDB extends BaseDB {
     public void createTable() throws SQLException {
         String query = "CREATE TABLE IF NOT EXISTS contact ("
                 + "id INT AUTO_INCREMENT PRIMARY KEY,"
-                + "email VARCHAR(255) NOT NULL,"
+                + "email VARCHAR(255) UNIQUE NOT NULL,"
                 + "view_link VARCHAR(40) NOT NULL,"
                 + "phone_number VARCHAR(40) NOT NULL,"
-                + "address VARCHAR(220) NOT NULL,"
-                + "birth_date DATE NOT NULL,"
-                + "fast_connect VARCHAR(40) NOT NULL,"
+                + "address VARCHAR(220),"
+                + "birth_date DATE,"
+                + "fast_connect VARCHAR(40),"
                 + "FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE"
                 + ");";
 
