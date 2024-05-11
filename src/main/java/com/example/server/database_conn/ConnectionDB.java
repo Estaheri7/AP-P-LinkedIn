feab955f961baa8e8c86c61bb7f75b2962f79a96
erfansalman <estaheri101@gmail.com>
2024-05-11 18:07:46 +0330

    add: added createTable method in ConnectionDB class

--- src/main/java/com/example/server/database_conn/ConnectionDB.java
+++ src/main/java/com/example/server/database_conn/ConnectionDB.java
@@ -1,6 +1,7 @@
 package com.example.server.database_conn;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionDB extends BaseDB {
 
@@ -10,6 +11,16 @@ public class ConnectionDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS connections ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "sender VARCHAR(255) NOT NULL,"
+                + "receiver VARCHAR(255) NOT NULL,"
+                + "commited BOOLEAN DEFAULT FALSE,"
+                + "FOREIGN KEY (sender) REFERENCES users(email) ON DELETE CASCADE,"
+                + "FOREIGN KEY (receiver) REFERENCES users(email) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
