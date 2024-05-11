e3be47296ced15f736c0fadda2203c0663500120
erfansalman <estaheri101@gmail.com>
2024-05-11 21:26:44 +0330

    add: added PostDB class

---
+++ src/main/java/com/example/server/database_conn/PostDB.java
@@ -0,0 +1,16 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PostDB extends BaseDB {
+
+    public PostDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
