df2198965c0aab19e77be45ae4b37ba3f98b6b9d
erfansalman <estaheri101@gmail.com>
2024-05-11 21:55:20 +0330

    add: added createTable method in LikeDB class

--- src/main/java/com/example/server/database_conn/LikeDB.java
+++ src/main/java/com/example/server/database_conn/LikeDB.java
@@ -1,6 +1,7 @@
 package com.example.server.database_conn;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LikeDB extends BaseDB {
 
@@ -10,6 +11,16 @@ public class LikeDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS likes("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "post_id INT NOT NULL,"
+                + "email VARCHAR(255) NOT NULL,"
+                + "like_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+                + "FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE,"
+                + "FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
