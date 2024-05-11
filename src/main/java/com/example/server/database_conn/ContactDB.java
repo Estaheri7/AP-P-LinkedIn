a5b92669849c213f525eb234f1471d53d5e2c296
erfansalman <estaheri101@gmail.com>
2024-05-11 16:21:52 +0330

    add: added createTable method in ContactDB class

--- src/main/java/com/example/server/database_conn/ContactDB.java
+++ src/main/java/com/example/server/database_conn/ContactDB.java
@@ -1,6 +1,7 @@
 package com.example.server.database_conn;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ContactDB extends BaseDB {
 
@@ -10,6 +11,18 @@ public class ContactDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS contact ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "email VARCHAR(255) NOT NULL,"
+                + "view_link VARCHAR(40) NOT NULL,"
+                + "phone_number VARCHAR(40) NOT NULL,"
+                + "address VARCHAR(220) NOT NULL,"
+                + "birth_date DATE NOT NULL,"
+                + "fast_connect VARCHAR(40) NOT NULL,"
+                + "FOREIGN KEY (email) REFERENCES users (email) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
