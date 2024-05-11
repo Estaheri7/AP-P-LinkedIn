f6c7df4f73ea722b4dfec9ae5913df7e8628fb54
erfansalman <estaheri101@gmail.com>
2024-05-11 16:11:41 +0330

    add: added createTable method in EducationDB class

--- src/main/java/com/example/server/database_conn/EducationDB.java
+++ src/main/java/com/example/server/database_conn/EducationDB.java
@@ -1,7 +1,8 @@
 package com.example.server.database_conn;
 
-import java.sql.Connection;
+
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class EducationDB extends BaseDB {
 
@@ -11,6 +12,20 @@ public class EducationDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS education ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "email VARCHAR(255) NOT NULL,"
+                + "school_name VARCHAR(40) NOT NULL,"
+                + "field VARCHAR(40) NOT NULL,"
+                + "grade FLOAT NOT NULL,"
+                + "start_date DATE NOT NULL,"
+                + "end_date DATE,"
+                + "community VARCHAR(500),"
+                + "description VARCHAR(1000),"
+                + "FOREIGN KEY (email) REFERENCES users (email)"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
