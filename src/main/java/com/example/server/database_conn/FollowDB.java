6b557eb46aa3e04eb3a362bed981b9ca52df10aa
erfansalman <estaheri101@gmail.com>
2024-05-11 17:51:45 +0330

    add: added FollowDB class

---
+++ src/main/java/com/example/server/database_conn/FollowDB.java
@@ -0,0 +1,16 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class FollowDB extends BaseDB {
+
+    public FollowDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+
+}
