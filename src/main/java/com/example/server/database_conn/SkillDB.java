9784783b7f46c46a61ee545c4a7cecb5f7173164
erfansalman <estaheri101@gmail.com>
2024-05-11 16:32:02 +0330

    add: added createTable method in SkillDB class

--- src/main/java/com/example/server/database_conn/SkillDB.java
+++ src/main/java/com/example/server/database_conn/SkillDB.java
@@ -1,6 +1,7 @@
 package com.example.server.database_conn;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SkillDB extends BaseDB {
 
@@ -10,6 +11,18 @@ public class SkillDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS skills ("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "email VARCHAR(255) UNIQUE NOT NULL,"
+                + "skill_1 VARCHAR(40),"
+                + "skill_2 VARCHAR(40),"
+                + "skill_3 VARCHAR(40),"
+                + "skill_4 VARCHAR(40),"
+                + "skill_5 VARCHAR(40),"
+                + "FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
 }
