6e4e1086ca233a2d22fab1dea5deb5b39a5d0429
erfansalman <estaheri101@gmail.com>
2024-05-11 16:23:46 +0330

    add: added SkillDB class

---
+++ src/main/java/com/example/server/database_conn/SkillDB.java
@@ -0,0 +1,15 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class SkillDB extends BaseDB {
+
+    public SkillDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
