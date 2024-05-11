4f65e5c1743f8864e2cad7c11e4e96f11850f96b
erfansalman <estaheri101@gmail.com>
2024-05-11 16:15:09 +0330

    add: added ContactDB class

---
+++ src/main/java/com/example/server/database_conn/ContactDB.java
@@ -0,0 +1,15 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class ContactDB extends BaseDB {
+
+    public ContactDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
