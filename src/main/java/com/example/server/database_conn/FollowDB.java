6cfbac91c258420fc586158e8902bfde0ac6cfad
erfansalman <estaheri101@gmail.com>
2024-05-11 17:58:11 +0330

    add: added createTable method in FollowDB class

--- src/main/java/com/example/server/database_conn/FollowDB.java
+++ src/main/java/com/example/server/database_conn/FollowDB.java
@@ -1,6 +1,7 @@
 package com.example.server.database_conn;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class FollowDB extends BaseDB {
 
@@ -10,7 +11,15 @@ public class FollowDB extends BaseDB {
 
     @Override
     public void createTable() throws SQLException {
+        String query = "CREATE TABLE IF NOT EXISTS follows("
+                + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                + "follower VARCHAR(255) NOT NULL,"
+                + "followed VARCHAR(255) NOT NULL,"
+                + "FOREIGN KEY (follower) REFERENCES users(email) ON DELETE CASCADE,"
+                + "FOREIGN KEY (followed) REFERENCES users(email) ON DELETE CASCADE"
+                + ");";
 
+        Statement statement = conn.createStatement();
+        statement.executeUpdate(query);
     }
-
 }
