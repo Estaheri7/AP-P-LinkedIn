a7692e329285ff0212e1b599de165236ddc9935d
erfansalman <estaheri101@gmail.com>
2024-05-11 21:57:23 +0330

    add: added HashtagDB class

---
+++ src/main/java/com/example/server/database_conn/HashtagDB.java
@@ -0,0 +1,15 @@
+package com.example.server.database_conn;
+
+import java.sql.SQLException;
+
+public class HashtagDB extends BaseDB {
+
+    public HashtagDB() throws SQLException {
+
+    }
+
+    @Override
+    public void createTable() throws SQLException {
+
+    }
+}
