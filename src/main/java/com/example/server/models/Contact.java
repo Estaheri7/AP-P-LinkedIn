95502ecc910888110b6b05c803e5f0d97f288f23
erfansalman <estaheri101@gmail.com>
2024-05-12 21:29:51 +0330

    add: added fields in Contact class in models package

--- src/main/java/com/example/server/models/Contact.java
+++ src/main/java/com/example/server/models/Contact.java
@@ -1,4 +1,13 @@
 package com.example.server.models;
 
+import java.util.Date;
+
 public class Contact {
+    private int id;
+    private String email;
+    private String viewLink;
+    private String phoneNumber;
+    private String address;
+    private Date birthDate;
+    private String fastConnect;
 }
