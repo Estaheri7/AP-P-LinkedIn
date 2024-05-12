311ad2d7c97c14c41dcca5705271514addfc679a
erfansalman <estaheri101@gmail.com>
2024-05-12 21:04:52 +0330

    add: added fields in Skill class in models package

--- src/main/java/com/example/server/models/Skill.java
+++ src/main/java/com/example/server/models/Skill.java
@@ -1,4 +1,11 @@
 package com.example.server.models;
 
 public class Skill {
+    private int id;
+    private String email;
+    private String skill1;
+    private String skill2;
+    private String skill3;
+    private String skill4;
+    private String skill5;
 }
