886916e532b833a402a78cf3b84abd0c178f8bf0
erfansalman <estaheri101@gmail.com>
2024-05-13 13:20:50 +0330

    add: added constructor, set,get methods in Like class in models package

--- src/main/java/com/example/server/models/Like.java
+++ src/main/java/com/example/server/models/Like.java
@@ -4,4 +4,33 @@ public class Like {
     private int id;
     private int postId;
     private String email;
+
+    public Like(int postId, String email) {
+        this.postId = postId;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
