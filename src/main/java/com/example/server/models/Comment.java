51f7472ae38d096ebb1903bec706059639c0ec2c
erfansalman <estaheri101@gmail.com>
2024-05-13 13:41:17 +0330

    add: added constructor, set,get methods in Comment class in models package

--- src/main/java/com/example/server/models/Comment.java
+++ src/main/java/com/example/server/models/Comment.java
@@ -5,4 +5,42 @@ public class Comment {
     private String postId;
     private String email;
     private String comment;
+
+    public Comment(String postId, String email, String comment) {
+        this.postId = postId;
+        this.email = email;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
