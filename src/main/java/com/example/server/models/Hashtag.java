04b7858addf03fb61dcf6724d4bf5ee58e173076
erfansalman <estaheri101@gmail.com>
2024-05-13 13:22:08 +0330

    add: added constructor set, get methods in Hashtag class in models package

--- src/main/java/com/example/server/models/Hashtag.java
+++ src/main/java/com/example/server/models/Hashtag.java
@@ -4,4 +4,33 @@ public class Hashtag {
     private int id;
     private int postId;
     private String hashtag;
+
+    public Hashtag(int postId, String hashtag) {
+        this.postId = postId;
+        this.hashtag = hashtag;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getHashtag() {
+        return hashtag;
+    }
+
+    public void setHashtag(String hashtag) {
+        this.hashtag = hashtag;
+    }
 }
