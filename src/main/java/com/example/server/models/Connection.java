e8291a30d76df889d98258dec474b8b67521be92
erfansalman <estaheri101@gmail.com>
2024-05-13 13:38:42 +0330

    add: added constructor, set,get methods in Connection class in models package

--- src/main/java/com/example/server/models/Connection.java
+++ src/main/java/com/example/server/models/Connection.java
@@ -4,4 +4,33 @@ public class Connection {
     private int id;
     private String sender;
     private String receiver;
+
+    public Connection(String sender, String receiver) {
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
 }
