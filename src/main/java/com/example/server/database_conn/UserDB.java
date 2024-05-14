e7c0ecc41670878b2c8ba9ef8abcba32152abaf0
erfansalman <estaheri101@gmail.com>
2024-05-15 00:33:58 +0330

    add: added insert,update,delete methods in UserDB in database_conn package

--- src/main/java/com/example/server/database_conn/UserDB.java
+++ src/main/java/com/example/server/database_conn/UserDB.java
@@ -1,6 +1,9 @@
 package com.example.server.database_conn;
 
+import com.example.server.models.User;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -29,5 +32,54 @@ public class UserDB extends BaseDB {
 
         Statement statement = conn.createStatement();
         statement.executeUpdate(query);
+    }
+
+    public void insertData(User user) throws SQLException {
+        String query = "INSERT INTO users (email, password, name, lastName, additionalName, avatar_url, background_url, headline, country, city)"
+                + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, user.getEmail());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getFirstName());
+        preparedStatement.setString(4, user.getLastName());
+        preparedStatement.setString(5, user.getAdditionalName());
+        preparedStatement.setString(6, user.getAvatar_url());
+        preparedStatement.setString(7, user.getBackground_url());
+        preparedStatement.setString(8, user.getHeadline());
+        preparedStatement.setString(9, user.getCountry());
+        preparedStatement.setString(10, user.getCity());
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateData(User user) throws SQLException {
+        String query = "UPDATE users SET email = ?, password = ?, name = ?, lastName = ?, additionalName = ?, avatar_url = ?, background_url = ?, headline = ?, country = ?, city = ? WHERE id = ?";
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, user.getEmail());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getFirstName());
+        preparedStatement.setString(4, user.getLastName());
+        preparedStatement.setString(5, user.getAdditionalName());
+        preparedStatement.setString(6, user.getAvatar_url());
+        preparedStatement.setString(7, user.getBackground_url());
+        preparedStatement.setString(8, user.getHeadline());
+        preparedStatement.setString(9, user.getCountry());
+        preparedStatement.setString(10, user.getCity());
+        preparedStatement.setInt(11, user.getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteData(int id) throws SQLException {
+        String query = "DELETE FROM users WHERE id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteAllData() throws SQLException {
+        String query = "DELETE FROM users";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
 }
