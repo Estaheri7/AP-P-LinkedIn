df05701b74332d4b219bce9a505c217ff8c4cc26
erfansalman <estaheri101@gmail.com>
2024-05-15 15:40:33 +0330

    add: added insert,update,delete methods in CommentDB class in database_conn package

--- src/main/java/com/example/server/database_conn/CommentDB.java
+++ src/main/java/com/example/server/database_conn/CommentDB.java
@@ -1,5 +1,8 @@
 package com.example.server.database_conn;
 
+import com.example.server.models.Comment;
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -23,5 +26,34 @@ public class CommentDB extends BaseDB {
 
         Statement statement = conn.createStatement();
         statement.executeUpdate(query);
+    }
+
+    public void insertData(Comment comment) throws SQLException {
+        String query = "INSERT INTO comments (post_id, email, message) VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, comment.getPostId());
+        preparedStatement.setString(2, comment.getEmail());
+        preparedStatement.setString(3, comment.getComment());
+    }
+
+    public void updateData(Comment comment) throws SQLException {
+        String query = "UPDATE comments SET comment = ? WHERE id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, comment.getComment());
+        preparedStatement.setInt(2, comment.getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteData(int id) throws SQLException {
+        String query = "DELETE FROM comments WHERE id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteAllData() throws SQLException {
+        String query = "DELETE FROM comments";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
 }
