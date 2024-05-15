3d8d1959410ffd37302bed8605d53740d91d7d2f
erfansalman <estaheri101@gmail.com>
2024-05-15 14:46:16 +0330

    add: added insert,update,delete methods in PostDB class in database_conn pacakge

--- src/main/java/com/example/server/database_conn/PostDB.java
+++ src/main/java/com/example/server/database_conn/PostDB.java
@@ -1,5 +1,8 @@
 package com.example.server.database_conn;
 
+import com.example.server.models.Post;
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,5 +27,36 @@ public class PostDB extends BaseDB {
 
         Statement statement = conn.createStatement();
         statement.executeUpdate(query);
+    }
+
+    public void insertData(Post post) throws SQLException {
+        String query = "INSERT INTO posts (email, title, content) VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, post.getAuthor());
+        preparedStatement.setString(2, post.getTitle());
+        preparedStatement.setString(3, post.getContent());
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateData(Post post) throws SQLException {
+        String query = "UPDATE posts SET title=?, content=? WHERE email=?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, post.getTitle());
+        preparedStatement.setString(2, post.getContent());
+        preparedStatement.setString(3, post.getAuthor());
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteData(int id) throws SQLException {
+        String query = "DELETE FROM posts WHERE id=?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteAllData() throws SQLException {
+        String query = "DELETE FROM posts";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
 }
