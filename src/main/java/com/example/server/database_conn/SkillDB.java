2b282b3ef315ced63b95fcfb22f597c28026a08a
erfansalman <estaheri101@gmail.com>
2024-05-15 14:38:02 +0330

    add: added insert,update,delete methods in SkillDB class in database_conn package

--- src/main/java/com/example/server/database_conn/SkillDB.java
+++ src/main/java/com/example/server/database_conn/SkillDB.java
@@ -1,5 +1,8 @@
 package com.example.server.database_conn;
 
+import com.example.server.models.Skill;
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,5 +27,43 @@ public class SkillDB extends BaseDB {
 
         Statement statement = conn.createStatement();
         statement.executeUpdate(query);
+    }
+
+    public void insertData(Skill skill) throws SQLException {
+        String query = "INSERT INTO skills (email, skill_1, skill_2, skill_3, skill_4, skill_5) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, skill.getEmail());
+        preparedStatement.setString(2, skill.getSkill1());
+        preparedStatement.setString(3, skill.getSkill2());
+        preparedStatement.setString(4, skill.getSkill3());
+        preparedStatement.setString(5, skill.getSkill4());
+        preparedStatement.setString(6, skill.getSkill5());
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateData(Skill skill) throws SQLException {
+        String query = "UPDATE skills SET email = ?, skill_1 = ?, skill_2 = ?, skill_3 = ?, skill_4 = ?, skill_5 = ? WHERE id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, skill.getEmail());
+        preparedStatement.setString(2, skill.getSkill1());
+        preparedStatement.setString(3, skill.getSkill2());
+        preparedStatement.setString(4, skill.getSkill3());
+        preparedStatement.setString(5, skill.getSkill4());
+        preparedStatement.setString(6, skill.getSkill5());
+        preparedStatement.setInt(7, skill.getId());
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteData(int id) throws SQLException {
+        String query = "DELETE FROM skills WHERE id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void deleteAllData() throws SQLException {
+        String query = "DELETE FROM skills";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
 }
