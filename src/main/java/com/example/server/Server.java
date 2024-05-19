e7aa63d1e743278756772bc3e337f213799fbfdc
erfansalman <estaheri101@gmail.com>
2024-05-19 16:38:48 +0330

    add: added constructor and createServer method in server class

--- src/main/java/com/example/server/Server.java
+++ src/main/java/com/example/server/Server.java
@@ -1,5 +1,26 @@
 package com.example.server;
 
-public class Server {
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+public class Server {
+    private HttpServer server;
+
+    public Server(int port) throws IOException {
+        createServer(port);
+    }
+
+    private void createServer(int port) throws IOException {
+        InetAddress localAddress = InetAddress.getByName("127.0.0.1");
+        this.server = HttpServer.create(new InetSocketAddress(localAddress, port), 0);
+
+        server.start();
+    }
+
+    public HttpServer getServer() {
+        return server;
+    }
 }
