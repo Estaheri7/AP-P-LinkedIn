4d2af4851bc1f62204eeb181e2bb820cee4d1498
erfansalman <estaheri101@gmail.com>
2024-05-19 17:32:30 +0330

    add: added getQueryParams method in Server class

--- src/main/java/com/example/server/Server.java
+++ src/main/java/com/example/server/Server.java
@@ -1,11 +1,13 @@
 package com.example.server;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class Server {
     private HttpServer server;
@@ -28,9 +30,11 @@ public class Server {
     public void get(String path, HttpHandler handler) {
         server.createContext(path, (exchange) -> {
             if (exchange.getRequestMethod().equals("GET")) {
+                HashMap<String, String> params = this.getQueryParams(exchange.getRequestURI().getQuery());
+                exchange.setAttribute("queryParams", params);
                 handler.handle(exchange);
             } else {
-
+                // TO-DO bad request error
             }
         });
     }
@@ -38,9 +42,11 @@ public class Server {
     public void post(String path, HttpHandler handler) {
         server.createContext(path, (exchange) -> {
             if (exchange.getRequestMethod().equals("POST")) {
+                HashMap<String, String> params = this.getQueryParams(exchange.getRequestURI().getQuery());
+                exchange.setAttribute("queryParams", params);
                 handler.handle(exchange);
             } else {
-
+                // TO-DO bad request error
             }
         });
     }
@@ -48,9 +54,11 @@ public class Server {
     public void delete(String path, HttpHandler handler) {
         server.createContext(path, (exchange) -> {
             if (exchange.getRequestMethod().equals("DELETE")) {
+                HashMap<String, String> params = this.getQueryParams(exchange.getRequestURI().getQuery());
+                exchange.setAttribute("queryParams", params);
                 handler.handle(exchange);
             } else {
-
+                // TO-DO bad request error
             }
         });
     }
@@ -58,10 +66,26 @@ public class Server {
     public void put(String path, HttpHandler handler) {
         server.createContext(path, (exchange) -> {
             if (exchange.getRequestMethod().equals("PUT")) {
+                HashMap<String, String> params = this.getQueryParams(exchange.getRequestURI().getQuery());
+                exchange.setAttribute("queryParams", params);
                 handler.handle(exchange);
             } else {
-
+                // TO-DO bad request error
             }
         });
+    }
+
+    private HashMap<String, String> getQueryParams(String query) {
+        HashMap<String, String> queryParams = new HashMap<>();
+
+        if (query != null) {
+            String[] params = query.split("&");
+            for (String param : params) {
+                int idx = param.indexOf("=");
+                queryParams.put(param.substring(0, idx), param.substring(idx + 1));
+            }
+        }
+
+        return queryParams;
     }
 }
