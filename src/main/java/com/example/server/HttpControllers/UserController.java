44fa23f8e1a8edb82ac1f1ee2954ea5115e2a328
erfansalman <estaheri101@gmail.com>
2024-05-22 21:26:01 +0330

    add: added getUser and getAllUsers in UserController class

--- src/main/java/com/example/server/HttpControllers/UserController.java
+++ src/main/java/com/example/server/HttpControllers/UserController.java
@@ -5,14 +5,32 @@ import com.example.server.database_conn.UserDB;
 import com.example.server.models.User;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UserController {
+    private static final UserDB userDB;
+
+    static {
+        try {
+            userDB = new UserDB();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void addUser(User user) throws SQLException  {
         if (!UserValidator.isValid(user)) {
             throw new IllegalArgumentException("Invalid data format");
         }
 
-        UserDB userDB = new UserDB();
         userDB.insertData(user);
+    }
+
+    public static User getUser(String email) throws SQLException {
+        return userDB.getUser(email);
+    }
+
+    public static ArrayList<User> getAllUsers() throws SQLException  {
+        return userDB.getAllUsers();
     }
 }
