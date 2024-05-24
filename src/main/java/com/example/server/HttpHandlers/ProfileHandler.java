cd9f7af30238f4f6f7a70587c1e66a37cf68589d
erfansalman <estaheri101@gmail.com>
2024-05-24 17:11:02 +0330

    add: created ProfileHandler class

---
+++ src/main/java/com/example/server/HttpHandlers/ProfileHandler.java
@@ -0,0 +1,11 @@
+package com.example.server.HttpHandlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+import static com.example.server.Server.extractEmailFromPath;
+
+public class ProfileHandler {
+
+}
