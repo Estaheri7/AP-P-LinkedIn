7272d35431c8a0d27575ff62a301372d8d1de7de
erfansalman <estaheri101@gmail.com>
2024-05-24 17:45:14 +0330

    add: added updateUser method in ProfileController class

--- src/main/java/com/example/server/HttpControllers/ProfileController.java
+++ src/main/java/com/example/server/HttpControllers/ProfileController.java
@@ -1,5 +1,36 @@
 package com.example.server.HttpControllers;
 
-public class ProfileController {
+import com.example.server.DataValidator.UserValidator;
+import com.example.server.database_conn.ContactDB;
+import com.example.server.database_conn.EducationDB;
+import com.example.server.database_conn.SkillDB;
+import com.example.server.database_conn.UserDB;
+import com.example.server.models.User;
 
+import java.sql.SQLException;
+
+public class ProfileController {
+    private static final UserDB userDB;
+    private static final SkillDB skillDB;
+    private static final EducationDB educationDB;
+    private static final ContactDB contactDB;
+
+    static {
+        try {
+            userDB = new UserDB();
+            skillDB = new SkillDB();
+            educationDB = new EducationDB();
+            contactDB = new ContactDB();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void updateUser(User user) throws SQLException {
+        if (!UserValidator.isValid(user)) {
+            throw new IllegalArgumentException("Invalid data format");
+        }
+
+        userDB.updateData(user);
+    }
 }
