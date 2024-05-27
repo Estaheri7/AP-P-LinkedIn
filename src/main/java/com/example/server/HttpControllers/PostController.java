136c6a15f57863f88e8537308bd0a15d22c897e3
erfansalman <estaheri101@gmail.com>
2024-05-27 13:06:48 +0330

    add: added addPost method in PostController class

--- src/main/java/com/example/server/HttpControllers/PostController.java
+++ src/main/java/com/example/server/HttpControllers/PostController.java
@@ -3,15 +3,16 @@ package com.example.server.HttpControllers;
 import com.example.server.database_conn.CommentDB;
 import com.example.server.database_conn.LikeDB;
 import com.example.server.database_conn.PostDB;
+import com.example.server.models.Post;
 
 import java.sql.SQLException;
 
 public class PostController extends BaseController {
-    private final PostDB postDB;
-    private final LikeDB likeDB;
-    private final CommentDB commentDB;
+    private static final PostDB postDB;
+    private static final LikeDB likeDB;
+    private static final CommentDB commentDB;
 
-    {
+    static {
         try {
             postDB = new PostDB();
             likeDB = new LikeDB();
@@ -19,5 +20,9 @@ public class PostController extends BaseController {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void addPost(Post post) throws SQLException {
+        postDB.insertData(post);
     }
 }
