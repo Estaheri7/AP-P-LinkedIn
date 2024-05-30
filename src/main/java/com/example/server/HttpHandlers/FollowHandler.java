0c4cf7e79577651f660d4f4609648ade25dea05d
mahdioudi <mahdioudi5@gmail.com>
2024-05-30 21:25:53 +0330

    add: added UnFollowHandler method to FollowHandler class

--- src/main/java/com/example/server/HttpHandlers/FollowHandler.java
+++ src/main/java/com/example/server/HttpHandlers/FollowHandler.java
@@ -55,5 +55,48 @@ public class FollowHandler {
         }
     }
 
+    public class UnFollowHandler {
+
+        private static final Gson gson = new Gson();
+
+        public static void followHandler(HttpExchange exchange) throws IOException {
+            String token = AuthUtil.getTokenFromHeader(exchange);
+            if (token == null || !AuthUtil.isTokenValid(exchange, token)) {
+                return;
+            }
+
+            String viewerEmail = JwtUtil.parseToken(token);
+            String requestEmail = extractEmailFromPath(exchange.getRequestURI().getPath());
+
+            if (!AuthUtil.isUserAuthorized(exchange, token, viewerEmail)) {
+                return;
+            }
+
+            try {
+                if (requestEmail == null || requestEmail.isEmpty()) {
+                    Server.sendResponse(exchange, 404, "User not found");
+                    return;
+                }
+
+                if (requestEmail.equals(viewerEmail)) {
+                    Server.sendResponse(exchange, 403, "You cannot unfollow yourself");
+                    return;
+                }
+                if (!FollowController.isFollowed(viewerEmail, requestEmail)) {
+                    Server.sendResponse(exchange, 403, "You already unfollowed this user");
+                    return;
+                }
+                FollowController.follow(viewerEmail, requestEmail);
+                Server.sendResponse(exchange, 200, "unFollowed");
+            } catch (IllegalArgumentException e) {
+                Server.sendResponse(exchange, 400, e.getMessage());
+            } catch (SQLException e) {
+                Server.sendResponse(exchange, 500, "Database error: " + e.getMessage());
+            } catch (Exception e) {
+                Server.sendResponse(exchange, 500, "Internal server error: " + e.getMessage());
+            }
+
+        }
+    }
 
 }
