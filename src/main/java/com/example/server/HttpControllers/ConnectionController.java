f58a39fecc0ecab5714940bb900ee77078023bad
mahdioudi <mahdioudi5@gmail.com>
2024-05-31 20:10:11 +0330

    add: added sendConnection method to ConnectionController class

---
+++ src/main/java/com/example/server/HttpControllers/ConnectionController.java
@@ -0,0 +1,61 @@
+package com.example.server.HttpControllers;
+
+import com.example.server.database_conn.ConnectionDB;
+import com.example.server.database_conn.FollowDB;
+import com.example.server.models.Connection;
+import com.example.server.models.Follow;
+import com.example.server.models.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ConnectionController extends BaseController {
+
+    private final static ConnectionDB connectionDB;
+    private final static FollowDB followDB;
+
+    static {
+        try {
+            connectionDB = new ConnectionDB();
+            followDB = new FollowDB();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<Connection> getConnections(String email) throws SQLException {
+        User user = userDB.getUser(email);
+        if (user == null) {
+            throw new IllegalArgumentException("User not found");
+        }
+        return connectionDB.getConnections(email);
+    }
+
+    public static ArrayList<Connection> getSenderNotification(String email) throws SQLException {
+        User user = userDB.getUser(email);
+        if (user == null) {
+            throw new IllegalArgumentException("User not found");
+        }
+            return connectionDB.getSenderNotification(email);
+    }
+
+    public static ArrayList<Connection> getReceiverNotification(String email) throws SQLException {
+        User user = userDB.getUser(email);
+        if (user == null) {
+            throw new IllegalArgumentException("User not found");
+        }
+        return connectionDB.getReceiverNotification(email);
+    }
+
+    public static void sendConnection(String sender, String receiver) throws SQLException {
+        User senderUser = userDB.getUser(sender);
+        User receiverUser = userDB.getUser(receiver);
+        if (senderUser == null || receiverUser == null) {
+            throw new IllegalArgumentException("User not found");
+        }
+
+        Connection connection = new Connection(sender, receiver);
+        connectionDB.insertData(connection);
+    }
+
+}
