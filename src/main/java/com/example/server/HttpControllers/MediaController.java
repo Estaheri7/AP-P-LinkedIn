5528abbb31b593fc27b8e0ffcf203d4fc591debe
erfansalman <estaheri101@gmail.com>
2024-06-02 01:55:23 +0330

    add: added addMediaToPost in MediaController

--- src/main/java/com/example/server/HttpControllers/MediaController.java
+++ src/main/java/com/example/server/HttpControllers/MediaController.java
@@ -1,14 +1,26 @@
 package com.example.server.HttpControllers;
 
 import com.example.server.CustomExceptions.NotFoundException;
+import com.example.server.database_conn.*;
 
 import java.sql.SQLException;
 
 public class MediaController extends BaseController {
+    public static final PostDB postDB;
+
+    static {
+        try {
+            postDB = new PostDB();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void updateAvatar(String email, String fileUrl) throws SQLException, NotFoundException {
         if (userDB.getUser(email) == null) {
             throw new NotFoundException("User not found");
         }
+
         userDB.uploadAvatar(email, fileUrl);
     }
 
@@ -16,6 +28,15 @@ public class MediaController extends BaseController {
         if (userDB.getUser(email) == null) {
             throw new NotFoundException("User not found");
         }
+
         userDB.uploadBackground(email, fileUrl);
+    }
+
+    public static void addMediaToPost(int postId, String fileUrl) throws SQLException, NotFoundException {
+        if (postDB.getPost(postId) == null) {
+            throw new NotFoundException("Post not found");
+        }
+
+        postDB.addMedia(postId, fileUrl);
     }
 }
