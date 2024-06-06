bcbeb18de003e4269225f04b79c09e5474056220
erfansalman <estaheri101@gmail.com>
2024-06-06 14:27:37 +0330

    add: added displayChatHandler in ChatHandler class

--- src/main/java/com/example/server/HttpHandlers/ChatHandler.java
+++ src/main/java/com/example/server/HttpHandlers/ChatHandler.java
@@ -13,6 +13,7 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static com.example.server.Server.extractFromPath;
 
@@ -41,6 +42,34 @@ public class ChatHandler {
             Server.sendResponse(exchange, 200, "Message sent successfully");
         } catch (NotFoundException e) {
             Server.sendResponse(exchange, 404, e.getMessage());
+        } catch (IllegalAccessError e) {
+            Server.sendResponse(exchange, 400, e.getMessage());
+        } catch (SQLException e) {
+            Server.sendResponse(exchange, 500, "Database error: " + e.getMessage());
+        } catch (Exception e) {
+            Server.sendResponse(exchange, 500, "Internal server error: " + e.getMessage());
+        }
+    }
+
+    public static void displayChatHandler(HttpExchange exchange) throws IOException {
+        String token = AuthUtil.getTokenFromHeader(exchange);
+        if (token == null || !AuthUtil.isTokenValid(exchange, token)) {
+            return;
+        }
+
+        String sender = JwtUtil.parseToken(token);
+        String receiver = extractFromPath(exchange.getRequestURI().getPath());
+        if (!AuthUtil.isUserAuthorized(exchange, token, sender)) {
+            return;
+        }
+
+        try {
+            ArrayList<Chat> chats = ChatController.displayChat(sender, receiver);
+            Server.sendResponse(exchange, 200, gson.toJson(chats));
+        } catch (NotFoundException e) {
+            Server.sendResponse(exchange, 404, e.getMessage());
+        } catch (IllegalAccessError e) {
+            Server.sendResponse(exchange, 400, e.getMessage());
         } catch (SQLException e) {
             Server.sendResponse(exchange, 500, "Database error: " + e.getMessage());
         } catch (Exception e) {
