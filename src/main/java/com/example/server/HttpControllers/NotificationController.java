0ef13b9c3d79378c2cc3879ff9a2289426eda072
erfansalman <estaheri101@gmail.com>
2024-07-03 18:22:38 +0330

    add: added NotificationController class

---
+++ src/main/java/com/example/server/HttpControllers/NotificationController.java
@@ -0,0 +1,27 @@
+package com.example.server.HttpControllers;
+
+import com.example.server.database_conn.NotificationDB;
+import com.example.server.models.Notification;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class NotificationController extends BaseController {
+    private final static NotificationDB notificationDB;
+
+    static {
+        try {
+            notificationDB = new NotificationDB();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addNotification(Notification notification) throws SQLException {
+        notificationDB.insertData(notification);
+    }
+
+    public static ArrayList<Notification> getAllNotifications(String email) throws SQLException {
+        return notificationDB.getAllNotifications(email);
+    }
+}
