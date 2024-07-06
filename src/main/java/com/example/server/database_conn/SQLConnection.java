2af59b77013cbb91ec84ebc11a357f6f889d13f9
erfansalman <estaheri101@gmail.com>
2024-07-06 16:12:23 +0330

    changed SQLConnection final fields to default

--- src/main/java/com/example/server/database_conn/SQLConnection.java
+++ src/main/java/com/example/server/database_conn/SQLConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/linkedin";
-    private static final String USER = "root";
-    private static final String PASSWORD = "123456";
+    private static final String URL = "jdbc:mysql://host:port/database";
+    private static final String USER = "username";
+    private static final String PASSWORD = "password";
 
     private static Connection conn = null;
 
